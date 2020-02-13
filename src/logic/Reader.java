05372bf2d3594d224daa4d9ec0c84fb7e2e9fb9a
20167014 <20167014@20167014.campus.tue.nl>
2020-02-13 10:57:19 +0100

    Implemented Reader 1, only data

--- src/logic/Reader.java
+++ src/logic/Reader.java
@@ -5,10 +5,17 @@
  */
 package logic;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+
+
 /**
  *
  *  This class turns the current solution into output that is accepted by
  *  momoter
+ * @author Stefan setup of class, implementation reader Leighton
  */
 public class Reader {
     
@@ -16,15 +23,82 @@ public class Reader {
     private int containerHeight;
     private boolean rotationsAllowed;
     private int[][] rectangles;
+    private String path;
     
-    public Reader() {
-        
+    public Reader(String path) {
+        this.path = path;
     }
     
-    public GlobalData read(){
+    /** reads the number of lines the input file has and returns it
+     * need to read the lines because array is faster implementation.
+     */
+    public int readLines() {
+        FileReader fileToRead = null;
+        BufferedReader bf = null;
+        try {
+        fileToRead = new FileReader(path);
+        bf = new BufferedReader(fileToRead);
+        }  
+        catch (FileNotFoundException e) {
+            System.out.println("Reader.readLines - File not found: " + path);
+        }
+        
+        String aLine;
+        int numberOfLines = 0;
+        try {
+            while (( aLine = bf.readLine()) != null ) {
+                numberOfLines++;
+            }
+            bf.close();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        
+ 
+        return numberOfLines;
+    }
+    
+    private String[] createArrayOfInput() {
+        FileReader fr = null;
+        BufferedReader textReader = null;
+        try{
+            fr = new FileReader(path);
+            textReader = new BufferedReader(fr);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Reader.read - File not found: " + path);
+        }
+        
+        int numberOfLines = readLines();
+        String[] textData = new String[numberOfLines];
+        
+        try {
+            for (int i = 0; i < numberOfLines; i++) {
+                textData[i] = textReader.readLine();
+            }
+            textReader.close();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        System.out.println("testing reader");
+        for (int i = 0; i < textData.length; i++){
+            System.out.println(textData[i]);
+        }
+        
+        return textData;
+    }
+    
+    /** reads input and returns it as GlobalData object */
+    public GlobalData read() {
         GlobalData data;
         // read input
-        // and store it in data
+        String[] textData = createArrayOfInput();
+        // store the input in data
+        
+        
         data = new GlobalData(containerType, containerHeight, rotationsAllowed
                                 , rectangles);
         return data;
