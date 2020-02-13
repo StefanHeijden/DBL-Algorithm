a8701840415cc63c19166b03e2dbf986c47eebc3
ezraleeuwenhage <ezra1999@hotmail.nl>
2020-02-13 13:44:13 +0100

    Changed rotations from list to boolean. Implemented Writer. Corrected column selection in TestingAlgorithm.

--- src/logic/Writer.java
+++ src/logic/Writer.java
@@ -17,6 +17,27 @@ public class Writer {
     public Writer(GlobalData data, Grid grid){
         this.data = data;
         this.grid = grid;
-        // output data
+    }
+    
+    // output data
+    public void writeOutput() {
+        
+        int[][] placement = grid.getPlacement();
+        System.out.println("placement of rectangles: ");
+        if (!data.getRA()) {
+            for (int[] i: placement) {
+                System.out.println(i[0] + " " + i[1]);
+            }
+        } else {
+            for (int i = 0; i < placement.length; i++) {
+                if (grid.getRotations()[i]) {
+                    System.out.println("yes" + placement[i][0] + " " + 
+                            placement[i][1]);
+                } else {
+                    System.out.println("no" + placement[i][0] + " " + 
+                            placement[i][1]);
+                }
+            }
+        }
     }
 }
