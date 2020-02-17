b961431e7d9110f320d043d110a0210cfd1317a1
YanaOnushkina <y.g.onushkina@student.tue.nl>
2020-02-17 13:48:17 +0100

    Testing out new algorithm

---
+++ src/algorithms/LevelPackingAlgorithm.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+import logic.GlobalData;
+import logic.Grid;
+/**
+ *
+ * @author yana
+ * This class places rectangles in a given layout
+ */
+public class LevelPackingAlgorithm extends AbstractAlgorithm {
+
+    public LevelPackingAlgorithm(Grid grid, GlobalData data) {
+        super(grid, data);
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
