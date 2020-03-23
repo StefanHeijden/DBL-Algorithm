6cc360b7cb21f3fd0af662f10ab3e1d4848daf04
Leighton van Gellecom <leightonvangellecom@gmail.com>
2020-03-23 18:07:45 +0100

    further implemented computeFNR(), made helper function removeRectangle(), implemented overlaps(), updated validPlacement(), added castRectangle as overlaps()' helper function

--- src/algorithms/BruteForceAlgorithm.java
+++ src/algorithms/BruteForceAlgorithm.java
@@ -2,6 +2,7 @@
 package algorithms;
 
 import java.awt.Rectangle;
+import java.util.ArrayList;
 import logic.GlobalData;
 import logic.Grid;
 
@@ -30,7 +31,7 @@ public class BruteForceAlgorithm extends AbstractAlgorithm{
     //used to initialize computations, of form [[[x0,y0], i0], [[x1,y1], i1]]
     //but with length of rectangles, i is for indexing the rectangle; x,y for width/height
     private int[][][] rectanglesWithIndex;
-    
+
     //method to run the algorithm, it will choose which variant to run in another method
     @Override
     public void run() {
@@ -45,7 +46,8 @@ public class BruteForceAlgorithm extends AbstractAlgorithm{
         // call the FNR variant 
         if (global.getType().equals(global.FREE) && ! global.getRA()) {
             //run with all rectangles, no current placment and origin as start corner
-            computeFNR(rectanglesWithIndex, null, originCorner, null);
+            //TODO: pass intialized length of currentPlacement
+            computeFNR(rectanglesWithIndex, originCorner, null);
         } 
         //call the FR variant
         else if (global.getType().equals(global.FREE) && global.getRA()) {
@@ -69,11 +71,15 @@ public class BruteForceAlgorithm extends AbstractAlgorithm{
     that still have to be placed. Second parameter: for the placed rectangles this
     is the placement in which they are placed. Third parameter stores the corners
     for which it tries all permutations, initialzation for this variant should be
-    with the origin (0, 0)**/
-    public void computeFNR(int[][][] rectanglesToPlace, Grid semiFinalPlacement, 
-            int[][] corners, int[][][] currentPlacement) {
-        //if Rectangles to place has no more rectangles to place
+    with the origin (0, 0)
+    currentPlacment is of form: [ [[x0,y0], i0], [[x1,y1], i0], [[x2,y2], i2] ]
+    where x an y are the placement coordinates and i is the rectangle index**/
+    public void computeFNR(int[][][] rectanglesToPlace, int[][] corners, int[][][] currentPlacement) {
+        //if Rectangles to place has no more rectangles to place, so done with placing
         if (! (rectanglesToPlace.length > 0)) {
+            //store results as grid object
+            Grid semiFinalPlacement = new Grid();
+            
             // compute the density of this placement
             semiFinalPlacement.computeFinalDensity(global);
             double density = semiFinalPlacement.getDensity();
@@ -91,19 +97,29 @@ public class BruteForceAlgorithm extends AbstractAlgorithm{
         //          place first rectangle
         //          and store in currentPlacement
         //          and update rectangles to place
-        //          FNR(RectanglesToPlace, currentPlacement);
-        //      else if rectangle cannot be placed at corner
-        //          do not place it, do not continue recursive calls
+        //          FNR(updated parameters);
+
         
         //loop over all corners
         for (int[] corner : corners) {
             //gets all possible permutations for rectangles that have not been placed
             int[][] permutations = computePermutations(rectanglesToPlace);
             for (int[] permutation : permutations) {
-                //stores the [x,y] where rectangle i is placed
-                currentPlacement[permutation[0]][0] = corner;
-                //stores the i of rectangle i
-                currentPlacement[permutation[0]][1][0] = permutation[0];
+                //if first rectangle of the permutation can be placed
+                if (validPlacement(permutation[0], corner, currentPlacement)) {
+                    //create new current placement
+                    int[][][] newCurrentPlacement = currentPlacement.clone();
+                    //stores the [x,y] where rectangle i is placed
+                    newCurrentPlacement[permutation[0]][0] = corner;
+                    //stores the i of rectangle i
+                    newCurrentPlacement[permutation[0]][1][0] = permutation[0];
+                    //update rectanglesToPlace by removing just placed rectangle
+                    int[][][] updatedRTBP = removeRectangle(permutation[0], rectanglesToPlace);
+                    //compute updated corners
+                    int[][] newCorners = computeCorners(corners, permutation[0], corner);
+                    //recursive call with updated parameters
+                    computeFNR(updatedRTBP, newCorners, newCurrentPlacement);
+                }
             }
         } 
     }
@@ -124,6 +140,31 @@ public class BruteForceAlgorithm extends AbstractAlgorithm{
     private Grid computeSPR() {
         Grid emptyGrid = new Grid();
         return emptyGrid;
+    }
+    
+    /** this method will remove a rectangle from the rectanglesToBePlaced,
+     * given the rectangle index as parameter and rectanglesToPlace, 
+     * it returns the updated 3d array, without adapting the input */
+    private int[][][] removeRectangle(int indexRectangleToRemove, int[][][] rectanglesToBePlaced) {
+        //representation for updated rectanglesToBePlaced
+        int[][][] updatedRTBP = new int[rectanglesToBePlaced.length - 1][][];
+        for (int i = 0; i < rectanglesToBePlaced.length; i++) {
+            //find where the rectangle is based on its index compute further with that stored in value i
+            if (indexRectangleToRemove == rectanglesToBePlaced[0][1][0]) {
+                //for loop to go copy values, note length is 1 shorter than previous loop
+                for (int j = 0; j < updatedRTBP.length; j++) {
+                    //case distinction based on whether index has been found
+                    if (j < i) { //rectangle not encountered
+                        updatedRTBP[j] = rectanglesToBePlaced[j];
+                    } else if (j == i) { //rectangle encountered
+                        updatedRTBP[j] = rectanglesToBePlaced[j + 1];
+                    } else { // j > i, so past to be removed rectangle
+                        updatedRTBP[j] = rectanglesToBePlaced[j + 1];
+                    }
+                }
+            }
+        }
+        return updatedRTBP; //return the updated rectangleToBePlaced, input not adapted
     }
     
     /* computes and returns 2d array of corners that can be filled. Initialization
@@ -186,7 +227,7 @@ public class BruteForceAlgorithm extends AbstractAlgorithm{
         //need to know how many rectangles to permutate for rectangles still to place
         int numRectangles = rectanglesToPlace.length;
         //need to get all the indexes of the rectangles to be permutated
-        int [] rectangleIndexes = new int[numRectangles];
+        int[] rectangleIndexes = new int[numRectangles];
         
         for (int i = 0; i < numRectangles; i++) { //loop over all rectangles to be placed
             //take out the index
@@ -199,18 +240,82 @@ public class BruteForceAlgorithm extends AbstractAlgorithm{
         return permutations;
     }
     
-    private boolean validPlacement(int IndexPlacedRectangle, int[] placementCoordinates) {
-        return (!overlaps() && withinBounds(IndexPlacedRectangle, placementCoordinates));
+    private boolean validPlacement(int toBePlacedRectangleIndex, int[] placementCoordinates, 
+            int[][][] currentPlacement) {
+        //overlaps() has to be false and withinBounds() has to be true for valid placement
+        return (!overlaps(toBePlacedRectangleIndex, placementCoordinates, currentPlacement)
+                && withinBounds(toBePlacedRectangleIndex, placementCoordinates));
     }
     
     //TODO needs to be adapted to allow for rotations
-    private boolean overlaps() {
-        //TODO implement, idea is to use Rectangle class of java awt
-        return true; //assume while not implemented placement is not valid
+    private boolean overlaps(int toBePlacedRectangleIndex, int[] placementCoordinates, 
+            int[][][] currentPlacement) {
+        //idea is to use Rectangle class of java awt, first cast it to a Rectangle object
+        if (! global.getRA()) { //if no rotations
+            //Rectangles for which intersection computations have to be done
+            ArrayList<Rectangle> rectanglesAWP = castRectangle(toBePlacedRectangleIndex, 
+                    placementCoordinates, currentPlacement);
+            
+            //compute whether anything intersects, basically same code as isLegal() 
+            //from DrawPanel method is public but does not have parameters, 
+            //therefore not used but basically duplicate, apart from handling result
+            for (Rectangle r1 : rectanglesAWP) {
+                for (Rectangle r2 : rectanglesAWP) {
+                    //if  rectangles intersect and not the same implies overlap
+                    if (r1 != r2 && r1.intersects(r2)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } else {
+            return true; //assume while not implemented placement is not valid
+        }
     }
     
     //TODO needs to be adapted to allow for rotations
-    private boolean withinBounds(int IndexPlacedRectangle, int[] placementCoordinates) {      
+    /** this method computes and casts the current placement (+ rectangle to be placed)
+     to ArrayList of rectangle objects and returns that*/
+    private ArrayList<Rectangle> castRectangle(int toBePlacedRectangleIndex, int[] placementCoordinates, 
+            int[][][] currentPlacement) {
+        //ArrayList of rectangle objects, used to check intersections at castRectangle()
+        ArrayList<Rectangle> currentPlacementRectanglesAWT = new ArrayList();
+        
+        // in following loop all currently placed rectangles are added
+        for (int[][] rectangle : currentPlacement) { //each rectangle, form: [[x,y],[i]]
+            //get required data for Rectangle class
+            int rectWidth = getWidthRectangleIndex(rectangle[1][0]);
+            int rectHeight = getHeightRectangleIndex(rectangle[1][0]);
+            
+            //rectangle class uses placement from top left corner, so x = x, y += height
+            int xTopLeft = rectangle[0][0];
+            int yTopLeft = rectangle[0][1] + rectHeight;
+            
+            //create new rectangle object with above parameters as initialization
+            Rectangle newRectangle = new Rectangle(xTopLeft, yTopLeft, rectWidth, rectHeight);
+            currentPlacementRectanglesAWT.add(newRectangle);
+        }
+        
+        //now still the to be placed rectangle has to be added, note that above
+        //placement should always be valid since it has already been placed.
+        int rectWidth = getWidthRectangleIndex(toBePlacedRectangleIndex);
+        int rectHeight = getHeightRectangleIndex(toBePlacedRectangleIndex);
+        
+        //rectangle class uses placement from top left corner, so x = x, y += height
+        int xTopLeft = placementCoordinates[0];
+        int yTopLeft = placementCoordinates[1] + rectHeight;
+        
+        //create the rectangle object with above values
+        Rectangle toBePlacedRectangleCasted = new Rectangle(xTopLeft, yTopLeft, rectWidth, rectHeight);
+        
+        //add the toBePlacedRectangle
+        currentPlacementRectanglesAWT.add(toBePlacedRectangleCasted);
+        
+        return currentPlacementRectanglesAWT;
+    }
+    
+    //TODO needs to be adapted to allow for rotations
+    private boolean withinBounds(int toBePlacedRectangleIndex, int[] placementCoordinates) {      
         //if placement corner is not in first quadrant or does not touch x- or y-axis  
         //and in first quadrant than some part not in first quadrant so not within bounds
         if (! (placementCoordinates[0] >= 0 && placementCoordinates[1] >= 0)) {
@@ -218,7 +323,7 @@ public class BruteForceAlgorithm extends AbstractAlgorithm{
         }
         
         //maximal y coordinate of given placed rectangle
-        int maxYOfRectangle = placementCoordinates[1] + getHeightRectangleIndex(IndexPlacedRectangle);
+        int maxYOfRectangle = placementCoordinates[1] + getHeightRectangleIndex(toBePlacedRectangleIndex);
         
         //if FNR variant
         if (global.getType().equals(global.FREE) && ! global.getRA()) {
