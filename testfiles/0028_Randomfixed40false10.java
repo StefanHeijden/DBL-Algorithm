58782f0930634aae2cafa855d40752c2aa12d9ec
stefan <stefanheijden1@gmail.com>
2020-03-31 20:11:50 +0200

    10 rectangles test files

---
+++ testfiles/0028_Randomfixed40false10.java
@@ -0,0 +1,13 @@
+container height: fixed 40
+rotations allowed: no
+number of rectangles: 10
+8 12
+2 3
+11 16
+10 5
+13 8
+23 19
+18 23
+10 9
+7 20
+9 3
---
+++ testfiles/0030_Randomfixed70false10.java
@@ -0,0 +1,13 @@
+container height: fixed 70
+rotations allowed: no
+number of rectangles: 10
+19 24
+23 22
+4 7
+25 20
+8 13
+2 16
+5 8
+20 12
+20 16
+14 16
